682dca59be08868b32069884261cf307430e0eab
Marquis <abahmarquis@gmail.com>
2025-01-03 07:03:37 -0800

    updated more files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,75 +9,36 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  vue: {
+    compilerOptions: {
+      isCustomElement: tag => tag === 'cursor-fx'
+    }
+  },
   plugins: ["~/plugins/aos.client.ts"],
-  modules: ['@kevinmarrec/nuxt-pwa'],
+  css: ["/assets/css/main.css"],
+  modules: ['@kevinmarrec/nuxt-pwa', "@nuxtjs/tailwindcss"],
   pwa: {
     workbox: {
       enabled: true
     },
     meta: {
-      title: "Buildr",
+      title: "CoMiles – Elevating Ride Experiences",
       author: "Marquis",
       mobileAppIOS: false,
       mobileApp: true,
       description:
-        "specialize in software development, product design, and bespoke solutions to help businesses innovate and thrive in the digital age.",
-      theme_color: "#27396B",
-      background_color: "#27396B",
-      display: "standalone",
-      start_url: "/",
-      nativeUI: true,
+        "Elevating Ride Experiences",
+      theme_color: "#27396B"
     },
     icon: {
       sizes: [64, 120, 144, 152, 192, 384, 512],
     },
     manifest: {
-      name: "Achilles Drill",
+      name: "CoMiles – Elevating Ride Experiences",
       lang: "fa",
-      useWebmanifestExtension: false,
     },
-    runtimeCaching: [
-      {
-        urlPattern: "https://fonts.googleapis.com/.*",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern: "https://fonts.gstatic.com/.*",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern: "https://cdn.snipcart.com/.*",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-      {
-        urlPattern:
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
-        handler: "cacheFirst",
-        method: "GET",
-        strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-      },
-    ],
+  },
+  app: {
+    baseURL: process.env.DEPLOYMENT_TARGET === "s3" ? '/landing-page/' : '/'
   }
-  // modules: [
-  //   '@nuxtjs/pwa'
-  // ],
-  // pwa: {
-  //   manifest: {
-  //     name: 'My Awesome Nuxt 3 PWA',
-  //     short_name: 'Nuxt3PWA',
-  //     lang: 'en',
-  //     useWebmanifestExtension: false,
-  //     display: 'standalone',
-  //     theme_color: '#4DBA87',
-  //     background_color: '#000000'
-  //   },
-  //   workbox: {
-  //     // Workbox options for customizing the service worker
-  //   }
-  // }
 });
