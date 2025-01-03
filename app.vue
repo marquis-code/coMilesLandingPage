647d0ddeeb6b2f71f3c639ce7d0bd412e49279c1
Marquis <abahmarquis@gmail.com>
2025-01-03 07:02:31 -0800

    updated code

--- app.vue
+++ app.vue
@@ -1,15 +1,36 @@
 <template>
-  <div id="app">
-    <NuxtPage />
-  </div>
+  <main>
+    <CursorWrapper>
+      <CoreToast class="fixed top-4 right-4 z-[9999999]" />
+    <NuxtLayout class="z-10">
+      <NuxtPage class="z-10" />
+    </NuxtLayout>
+  </CursorWrapper>
+  </main>
 </template>
 
-<style lang="css">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+<script setup lang="ts">
+import { provide } from 'vue';
+import { visible, toastData } from '@/composables/core/useCustomToast';
 
-  #app {
-    @apply min-h-screen font-sans;
-  }
+// Provide the toast state globally
+provide('toastVisible', visible);
+provide('toastData', toastData);
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bangers&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Baskervville&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
 </style>
